52d7c6e7b863ec41cdf6c0e4d77e8a3ee49486ef
clkong <36968245+clkong@users.noreply.github.com>
2018-03-28 13:46:14 -0400

    Update GuessingGame.java

--- src/guessinggame/GuessingGame.java
+++ src/guessinggame/GuessingGame.java
@@ -1,7 +1,7 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Sean Luo 27/03/2018
+ * GuessingGame.java
+ * This program calculates the GUessingGame
  */
 
 package guessinggame;
